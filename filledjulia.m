4af561b063e38497207dc5bbe5961695d3ac7ed8
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-02-19 10:54:19 -0800

    Add files via upload

--- filledjulia.m
+++ filledjulia.m
@@ -2,8 +2,10 @@
 %  iteration remains bounded) for the function phi.
 
 phi = inline('z^2 - 1.25');   % Define the function whose fixed points we seek.
-fixpt1 = (1 + sqrt(6))/2;     % These are the fixed points.
-fixpt2 = (1 - sqrt(6))/2;
+syms x
+eqn=x^2-1.25==x;
+a = solve(eqn,x);     % These are the fixed points.
+fixpt1=a(1);fixpt2=a(2);
 
 colormap([1 0 0; 1 1 1]);     % Points numbered 1 (inside) will be colored red;
                               %   those numbered 2 (outside) will be colored white.
@@ -15,14 +17,23 @@ for j=1:71                 % Try initial values with imaginary parts between
     x = -1.8 + (i-1)*.02;     %   -1.8 and 1.8.
     z = x + 1i*y;             % 1i is the MATLAB symbol for sqrt(-1).
     zk = z;
+    iflag1=0;
+    iflag2=0;
     kount = 0;                % kount is the total number of iterations.
 
-    while kount < 30 & abs(zk) < 2
+    while kount < 100 & abs(zk) < 2 & iflag1 < 5 & iflag2 < 5
       kount = kount+1;
       zk = phi(zk);           % This is the fixed point iteration.
+      
+      err1=abs(zk-fixpt1); %Test for convergence to fixpt1
+      if err1<1.e-6,iflag1=iflag1+1;else,iflag1=0;end;
+      
+      err2=abs(zk-fixpt2); %Test for convergence to fixpt2
+      if err2<1.e-6,iflag2=iflag2+1;else,iflag2=0;end;
+      
     end
 
-    if abs(zk) < 2           % If orbit is bounded, set this
+    if iflag1 >=5 | iflag2 >=5 | kount>=100           % If orbit is bounded, set this
       M(j,i) = 1;             %   point color to 1 (red).
     end
 
