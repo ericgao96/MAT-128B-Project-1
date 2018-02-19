f63e665cf7292b0a45be3bac023d40eb4ff19dba
UCDJLei <36607613+UCDJLei@users.noreply.github.com>
2018-02-19 14:12:16 -0800

    Add files via upload
    I add a condition, which is the absolute value of z should be 1, and I  got the graph, but I don't know whether I am logically right.

---
+++ juliaset v_2.m
@@ -0,0 +1,40 @@
+%  Computes the filled Julia set (the initial points from which fixed point
+%  iteration remains bounded) for the function phi.
+
+phi = inline('z^2');   % Define the function whose fixed points we seek.
+syms x
+eqn=x^2==x;
+a = solve(eqn,x);     % These are the fixed points.
+fixpt1=a(1);fixpt2=a(2);
+
+colormap([1 0 0; 1 1 1]);     % Points numbered 1 (inside) will be colored red;
+                              %   those numbered 2 (outside) will be colored white.
+M = 2*ones(201,201);          % Initialize array of point colors to 2 (white).
+
+for j=1:201                 % Try initial values with imaginary parts between
+  y = -2 + (j-1)*.02;        %   -2 and 2
+  for i=1:201              % and with real parts between
+    x = -2 + (i-1)*.02;     %   -2 and 2
+    z = x + 1i*y;             % 1i is the MATLAB symbol for sqrt(-1).
+    zk = z;
+    kount = 0;                % kount is the total number of iterations.
+
+    while kount < 30 & abs(zk) < 2
+      kount = kount+1;
+      zk = phi(zk);           % This is the fixed point iteration.
+    end
+
+    if abs(zk) < 2   % If orbit is bounded, set this
+      % based on filled julia set, we find the boundary
+       if abs(z) == 1
+        
+        M(j,i) = 1;             %   point color to 1 (red).
+       end
+       
+       end
+
+  end
+end
+
+image([-2 2],[-2 2],M),  % This plots the results.
+axis xy                        % If you don't do this, vertical axis is inverted.
