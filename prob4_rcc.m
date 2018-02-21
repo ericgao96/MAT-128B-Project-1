8314a85748225316418fccdfdc7e56623942b50d
ericgao96 <36257172+ericgao96@users.noreply.github.com>
2018-02-20 19:16:07 -0800

    format editing

--- prob4_rcc.m
+++ prob4_rcc.m
@@ -6,25 +6,25 @@ X=imread(imageName);   % Transform the image into 0-1 grayscale image.
 n=graythresh(X);       % 1 stands for the existence of points.
 X=im2bw(X,n);
 
-if size(X,1)~=size(X,2)  %Check whetehr M=N
+if size(X,1)~=size(X,2)                  %Check whetehr M=N
    error('The size of X must NxN.'); 
 end
 
 B=size(X,1);temp=B;h=1;N(h)=0;L(h)=0;
 
-while temp>1                % temp=1 means each small grid is a pixel. There is no need to continue.
+while temp>1                             % temp=1 means each small grid is a pixel. There is no need to continue.
     temp2=0;
-    for i=2:99              % select the value of L
+    for i=2:99                           % select the value of L
         if mod(temp,i)==0
             L(h)=temp/i;temp=L(h);
             break
         end
     end
    
-    for j=1:(B/L(h))       % locate the area of each part of the grid, j is by row, k is by column.
+    for j=1:(B/L(h))                                               % locate the area of each part of the grid, j is by row, k is by column.
         for k=1:(B/L(h))
             area=X([L(h)*(j-1)+[1:L(h)],L(h)*(k-1)+[1:L(h)]]);
-            if max(area(1:end))==1    % "1" means the small grid contains at least one point of julia sets
+            if max(area(1:end))==1                                 % "1" means the small grid contains at least one point of julia sets
                 temp2=temp2+1;
             end
         end
